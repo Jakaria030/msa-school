790c2b9513f084b9844a864e87c10840c83a6e69
Jakaria030 <gulamjakaria99@gmail.com>
2024-11-05 01:53:46 +0600

    Carousel deploy related issue fixed

--- swiper.js
+++ swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-slider', {
+const swiper1 = new Swiper('.swiper-slider-1', {
     // Optional parameters
     loop: true,
     grabCursor: true,
@@ -6,14 +6,84 @@ const swiper = new Swiper('.swiper-slider', {
   
     // Circle pagination 
     pagination: {
-      el: '.swiper-pagination',
+      el: '.pagination-1',
       clickable: true,
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.next-1',
+      prevEl: '.prev-1',
+    },
+    
+    // break point
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+        1440:{
+            slidesPerView: 4,
+        },
+    },
+});
+
+const swiper2 = new Swiper('.swiper-slider-2', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // Circle pagination 
+    pagination: {
+      el: '.pagination-2',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next-2',
+      prevEl: '.prev-2',
+    },
+    
+    // break point
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+        1440:{
+            slidesPerView: 4,
+        },
+    },
+});
+
+const swiper3 = new Swiper('.swiper-slider-3', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // Circle pagination 
+    pagination: {
+      el: '.pagination-3',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next-3',
+      prevEl: '.prev-3',
     },
     
     // break point
